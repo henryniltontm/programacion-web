4c943cdfa90c24a2b9e6fc0e84b0c70ace48d0fc
henry <henry.6f@hotmail.com>
2016-08-09 19:25:51 -0500

    agregamos presentacion y comportamiento

--- hola-php.php
+++ hola-php.php
@@ -3,9 +3,37 @@
 <html>
 <head>
 	<title></title>
+	<style type="text/css">
+		h1{color:peru;}
+		p{color:#adcbf7;}
+		li:first-letter{font-size:2em;}
+	</style>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
 	<h1>HOLA PHP</h1>
+	<p>Dias de la semana</p>
+	<a class="btn btn-primary" href="http://www.google.com">Ir a google </a>
+	<a class="btn btn-primary" href="http://www.youtube.com">Ir a youtube </a>
+	<ol>
+		<li>Lunes</li>
+		<li>Martes</li>
+		<li>Miercoles</li>
+		<li>Jueves</li>
+		<li>Viernes</li>
+		<li>Sabado</li>
+		<li>Domingo</li>
+	</ol>
+	<script type="text/javascript">
+		alert("Hola:"+prompt("Ingresa tu nombre"));
+	</script>
 
 </body>
 </html>
